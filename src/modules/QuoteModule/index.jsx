30aeccedf60a09ec06a14c218b00c4a68859d482
Diyor <77630836+Diyor11@users.noreply.github.com>
2023-09-05 14:50:52 +0500

    Update index.jsx

--- src/modules/QuoteModule/index.jsx
+++ src/modules/QuoteModule/index.jsx
@@ -1,7 +1,7 @@
 import { ErpLayout } from '../../layout';
 import ErpPanel from '../../components/ErpPanel';
 import QuoteForm from './QuoteForm';
-import dataTableDropMenu from './DataTableDropMenu';
+import dataTableDropMenu from './dataTableDropMenu';
 
 export default function InvoiceModule({ config }) {
   return (
